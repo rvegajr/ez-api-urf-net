4f4b4d3d15539794500ce187055ca157237c4aae
Ricardo Vega <github@noctusoft.com>
2018-09-15 16:29:01 -0500

    Object Name will remove a space as specifed in ezdbcodegen.config.json

--- Src/EzApi.Web/Properties/AssemblyInfo.cs
+++ Src/EzApi.Web/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.26.1450")]
-[assembly: AssemblyFileVersion("1.0.26.1450")]
+[assembly: AssemblyVersion("1.0.27.2123")]
+[assembly: AssemblyFileVersion("1.0.27.2123")]
