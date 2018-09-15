21b1535897c8e4c5555ba0cb7fc3cb8e684feb97
Ricardo Vega <github@noctusoft.com>
2018-09-15 09:53:54 -0500

    Baseline functional checkin!

--- Src/EzApi.Web/Properties/AssemblyInfo.cs
+++ Src/EzApi.Web/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.23.2246")]
-[assembly: AssemblyFileVersion("1.0.23.2246")]
+[assembly: AssemblyVersion("1.0.26.1450")]
+[assembly: AssemblyFileVersion("1.0.26.1450")]
